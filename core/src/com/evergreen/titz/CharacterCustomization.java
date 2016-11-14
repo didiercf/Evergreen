5f798fc4adc513468290f76a662825930d877e11
apollo <didiercamous@gmail.com>
2016-11-14 09:16:53 -0500

    Core class now extends Game instead of ApplicationAdapter, gonna start working with screens

--- core/src/com/evergreen/titz/CharacterCustomization.java
+++ core/src/com/evergreen/titz/CharacterCustomization.java
@@ -1,14 +1,13 @@
 package com.evergreen.titz;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class CharacterCustomization extends ApplicationAdapter implements InputProcessor {
+public class CharacterCustomization extends Game {
 
 	SpriteBatch batch;
 	TextureRegion currentFrame;
@@ -54,45 +53,5 @@ public class CharacterCustomization extends ApplicationAdapter implements InputP
 	public void dispose () {
 		batch.dispose();
 		animation.dispose();
-	}
-
-	@Override
-	public boolean keyDown(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyUp(int keycode) {
-		return false;
-	}
-
-	@Override
-	public boolean keyTyped(char character) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-		return false;
-	}
-
-	@Override
-	public boolean touchDragged(int screenX, int screenY, int pointer) {
-		return false;
-	}
-
-	@Override
-	public boolean mouseMoved(int screenX, int screenY) {
-		return false;
-	}
-
-	@Override
-	public boolean scrolled(int amount) {
-		return false;
 	}
 }
