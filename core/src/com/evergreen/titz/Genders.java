120a0938f0cc2c81cfe8ed8be062df0491e39685
Frederic Deschenes <frederic.deschenes1@gmail.com>
2016-11-22 22:45:43 -0500

    Something like this

--- core/src/com/evergreen/titz/Genders.java
+++ core/src/com/evergreen/titz/Genders.java
@@ -4,12 +4,13 @@ package com.evergreen.titz;
  * Created by apollo on 11/22/2016.
  */
 public enum Genders {
-    GENDERS("male", "female", "genderfluid");
+    MALE("male"),
+    FEMALE("female"),
+    GENDER_FLUID("genderfluid");
+    
+    public final String friendlyName; 
 
-    public String MALE, FEMALE, GENDER_FLUID;
-
-    Genders(String male, String female, String genderFluid) {
-        MALE = male;
-        FEMALE = female;
+    Genders(String name) {
+        this.friendlyName = name;
     }
 }
